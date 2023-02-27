dfe57046065e92bcfa2b1fc5f44b941dd6e126ae
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-02-27 00:08:00 -0300

    feat: Added initial handling for controlling the game navigation flow

--- mini-projects/mentalist/src/js/script.js
+++ mini-projects/mentalist/src/js/script.js
@@ -1,1 +1,17 @@
-alert('You\'re on the browser');
+const waitStart = document.querySelector('.start-game');
+const form = document.getElementById('interval-form');
+
+waitStart.querySelector('button').addEventListener('click', function() {
+	
+	let formControl = form.parentElement;
+
+	waitStart.className = 'start-game success';
+	formControl.className = 'container success';
+});
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	
+	let formControl = form.parentElement;
+	formControl.className = 'container remove';
+});
