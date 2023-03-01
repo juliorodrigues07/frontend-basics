cdebe40368a320ea5a90c7a2439f121b70f5c998
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-03-01 01:15:40 -0300

    update: Added a restart-game button and some explanatory comments

--- mini-projects/mentalist/src/js/script.js
+++ mini-projects/mentalist/src/js/script.js
@@ -1,32 +1,50 @@
 // TODO: Add navigation between stages
+
+// Stage 1: Button to start the game
 const waitStart = document.querySelector('.start-game');
 
+// Alternative flow: Button to reset the game in any stage
+const restart = document.querySelector('.restart-game');
+
+// Stage 2: Defining the random number generator interval
 const form = document.getElementById('interval-form');
 const fromInput = document.getElementById('from');
 const toInput = document.getElementById('to');
 
+// Stage 3: Guessing the number
 const guessing = document.getElementById('input-form');
 let guess = document.getElementById('input');
 let begin, end, randomNumber;
 
+// On click, removes 'start' button and shows the interval definition form, along with the restart button
 waitStart.querySelector('button').addEventListener('click', function() {
 	
 	let formControl = form.parentElement;
 
 	waitStart.className = 'start-game success';
 	formControl.className = 'container add';
+	restart.className = 'restart-game add';
 });
 
+// If the restart button is clicked anywhere during the game, it reloads the page, bringing back to stage 1
+restart.querySelector('button').addEventListener('click', function() {
+	location.reload();
+});
+
+// On click for submiting the interval, the inputs are checked, to then start (or not) the guessing (stage 3)
 form.addEventListener('submit', (e) => {
 	
 	e.preventDefault();
 	let check; 
 	[check, randomNumber] = checkIntervalInputs();
 
+	// If the data is valid
 	if (check) {
+		// Removes the interval form
 		let formControl = form.parentElement;
 		formControl.className = 'container remove';
-
+		
+		// Adds the guessing form to receive attempts
 		let inputControl = guessing.parentElement;
 		inputControl.className = 'try add';
 	}
@@ -34,6 +52,7 @@ form.addEventListener('submit', (e) => {
 
 guessing.addEventListener('submit', (f) => {
 
+	// Validates (or not) the guesses, doing similar checks as stage 2
 	f.preventDefault();
 	checkGuessInput();
 });
@@ -43,6 +62,7 @@ function checkIntervalInputs() {
 	begin = fromInput.value;
 	end = toInput.value;
 	
+	// The beginning interval value needs to be a number and smaller than the ending interval value 
 	if (begin === '')
 		setErrorFor(fromInput, 'Inform a value.');
 	else if (isNaN(begin))
@@ -52,6 +72,7 @@ function checkIntervalInputs() {
 	else
 		setSuccessFor(fromInput);
 	
+	// Ending value also has to be a number, but greater than the beginning value
 	if (end === '')
 		setErrorFor(toInput, 'Inform a value.');
 	else if (isNaN(end))
@@ -61,12 +82,15 @@ function checkIntervalInputs() {
 	else
 		setSuccessFor(toInput);
 
+
+	// Checks if the data present in the entire form ia valid
 	const formControls = form.querySelectorAll('.form-control');
 	
 	const formIsValid = [...formControls].every(formControl => {
 		return (formControl.className == 'form-control success');
 	});
 	
+	// Generates (or not) the random number accordingly to the interval passed
 	if (formIsValid) {
 		let randomNumber = cryptoRandom(Number(begin), Number(end));
 		return [true, randomNumber];
@@ -76,6 +100,7 @@ function checkIntervalInputs() {
 
 function cryptoRandom(minValue, maxValue) {
 	
+	// Generate a random number between a range [a, b], with max(min) bound = (-)2^31 - 1 (two's complement lang's repr)
 	let value = minValue + (maxValue - minValue + 1) * crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32 | 0;
 	return value;
 }
@@ -84,6 +109,7 @@ function checkGuessInput() {
 
 	const attempt = guess.value;
 
+	// An attempt has to be number and be between the interval passed at stage 2 [min, max]
 	if (attempt === '')
 		setErrorFor(guess, 'Inform a value.');
 	else if (isNaN(attempt))
@@ -110,6 +136,7 @@ function validateAttempt(attempt) {
 	let numberOfAttempts = document.getElementById('inner').innerText;
 	let history = document.getElementById('logs').innerText;
 	
+	// Show some hints accordingly to the values passed
 	if (yourTry == randomNumber) 
 		document.getElementById('logs').innerText = '\nYOU FOUND IT!!!' + history;
 	else if (yourTry < randomNumber) {
@@ -120,6 +147,7 @@ function validateAttempt(attempt) {
 		document.getElementById('logs').innerText = message + history;
 	}
 
+	// Increments the number of attempts as each request is made
 	document.getElementById('inner').innerText = Number(numberOfAttempts) + 1;
 }
 
