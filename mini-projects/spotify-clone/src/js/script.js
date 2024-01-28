6e32622a596fd3465fc072bc0f1cfa481aa4e8d4
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2024-01-27 21:40:27 -0300

    Feat: Search now can display multiple results
    Now the DOM is modified dinamically with proper methods. Also, at each event the grid is cleansed to append the new results. Otherwise, the grid would overflow with search results, acumulating all gathered data during searching.

--- mini-projects/spotify-clone/src/js/script.js
+++ mini-projects/spotify-clone/src/js/script.js
@@ -3,6 +3,13 @@ const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 
+function removeAllChildNodes(parent) 
+{
+	while (parent.firstChild)
+		parent.removeChild(parent.firstChild);
+}
+
+
 async function requestAPI(searchTerm)
 {
 	const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
@@ -14,16 +21,66 @@ async function requestAPI(searchTerm)
 
 function displayResults(data)
 {
+	// Hides the playlist grid
 	resultPlaylist.classList.add('hidden');
 
-	const artistName = document.getElementById('artist-name');
-	const artistImage = document.getElementById('artist-img');
+	// Removes possible results from previous seaches 
+	let newCard = document.getElementById('artists-grid');
+	removeAllChildNodes(newCard);
 
 	data.forEach(element => {
+
+		// Creates the card's img component
+		let artistImg = document.createElement('img');
+		artistImg.id = 'artist-img';
+		artistImg.classList.add('artist-img');
+		artistImg.src = element.urlImg;
+	
+		// Creates the play button and its icon (hover) 
+		let buttonIcon = document.createElement('span');
+		buttonIcon.className = 'fa fa-solid fa-play';
+		let playButton = document.createElement('div');
+		playButton.classList.add('play');
+		playButton.appendChild(buttonIcon);
+	
+		let newImg = document.createElement('div');
+		newImg.classList.add('card-img');
+		newImg.appendChild(artistImg);
+		newImg.appendChild(playButton);
+	
+		// Creates the card text content (external link, name and category)
+		let linkableArtist = document.createElement('a');
+		linkableArtist.title = '';
+		linkableArtist.classList.add('vst');
+		linkableArtist.href = '';
+	
+		let artistName = document.createElement('span');
+		artistName.classList.add('artist-name');
+		artistName.id = 'artist-name';
 		artistName.innerText = element.name;
-		artistImage.src = element.urlImg;
+	
+		let category = document.createElement('span');
+		category.classList.add('category');
+		category.innerText = 'Artista';
+	
+		let newText = document.createElement('div');
+		newText.classList.add('card-text');
+		newText.appendChild(linkableArtist);
+		newText.appendChild(artistName);
+		newText.appendChild(category);
+	
+		// Creates the whole card to be appended on the grid
+		let artistCard = document.createElement('div');
+		artistCard.classList.add('artist-card');
+		artistCard.id = 'artist-card';
+		artistCard.appendChild(newImg);
+		artistCard.appendChild(newText);
+		
+		// Appends the search result card to the grid on the DOM
+		newCard.appendChild(artistCard);
 	});
 
+	// Turn the search result grid visible
 	resultArtist.classList.remove('hidden');
 }
 
